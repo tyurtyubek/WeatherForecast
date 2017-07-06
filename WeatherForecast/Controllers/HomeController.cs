0a439c2ba7cbcc06b16142fe0e1d8f20378c88ff
tyurtyubek <tyurtyubek@outlook.com>
2017-07-06 20:14:28 +0300

    finish hometask

--- WeatherForecast/Controllers/HomeController.cs
+++ WeatherForecast/Controllers/HomeController.cs
@@ -3,28 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WeatherForecast.Services;
 
 namespace WeatherForecast.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly List<string> cities = new List<string> { "Kiev", "Lviv", "Kharkiv", "Dnipropetrovsk", "Odessa" };
         public ActionResult Index()
         {
+            var cityoflist = new List<SelectListItem>();
+            foreach (var city in cities)
+            {
+                var selectListItem = new SelectListItem
+                {
+                    Value = city,
+                    Text = city
+                };
+                cityoflist.Add(selectListItem);
+            }
+            ViewBag.Cities = cityoflist;
+            var daysnumbSelect = new List<SelectListItem>
+            {
+                new SelectListItem
+                {
+                    Text = "weather for 1 day",
+                    Value = "1",
+                    Selected = true
+                },
+                new SelectListItem
+                {
+                    Text = "weather for 3 days",
+                    Value = "3"
+                },
+                new SelectListItem
+                {
+                    Text = "weather for 7 days",
+                    Value = "7",
+                },
+            };
+            ViewBag.DaysNumb = daysnumbSelect;
             return View();
         }
 
-        public ActionResult About()
+
+        // GET: Home/GetForecast
+        [HttpGet]
+        public ActionResult GetForecast(string cityoflist, string cityOwn, string daysnumb)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            WeatherForecastProvider weather = new WeatherForecastProvider();
+            var model = weather.GetWeatherForecastJson(cityoflist, Convert.ToInt32(daysnumb), cityOwn);
+            return View("GetForecast", model);
+            return model == null ? View("Error") : View("GetForecast", model);
         }
     }
 }
---
+++ WeatherForecast/Services/WeatherForecastProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using WeatherForecast.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Http;
+
+namespace WeatherForecast.Services
+{
+    public class WeatherForecastProvider
+    {
+        private readonly string apiKey = "421cd20af77e3b17cb60061874c5c88f";
+
+        public RootObject GetWeatherForecastJson(string cityoflist , int days, string cityOwn)
+        {
+            string city = "";
+            if (cityOwn == "")
+                city = cityoflist;
+            else city = cityOwn;
+            string openwmApiUrl =  $"http://api.openweathermap.org/data/2.5/forecast/daily?q={city}&cnt={days}&APPID={apiKey}&units=metric";
+            var client = new HttpClient();
+            var json = client.GetStringAsync(openwmApiUrl).Result;
+            return JsonConvert.DeserializeObject<RootObject>(json);
+        }
+    }
+}
