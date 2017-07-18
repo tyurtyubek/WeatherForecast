496dd23f4f6532fd03b9d860d0984bbbfcf317d2
tyurtyubek <tyurtyubek@outlook.com>
2017-07-18 20:38:14 +0300

    Added HistoryController and WeatherController

---
+++ WeatherForecast/API/HistoryController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using WeatherForecast.Context;
+using WeatherForecast.Models;
+
+namespace WeatherForecast.API
+{
+    public class HistoryController : ApiController
+    {
+        private ICityLogRepository _citySearch;
+
+        public HistoryController(ICityLogRepository citySearch)
+        {
+            _citySearch = citySearch;
+        }
+
+        //Get /api/history
+        public IEnumerable<CityLog> Get()
+        {
+            return _citySearch.GetCitiesLog();       
+        }
+
+
+
+    }
+}
--- WeatherForecast/API/WeatherController.cs
+++ WeatherForecast/API/WeatherController.cs
@@ -15,19 +15,34 @@ namespace WeatherForecast.API
     public class WeatherController: ApiController
     {
         private IWeatherForecastProvider _provider;
+        private ICityLogRepository _citySearch;
 
-        public WeatherController(IWeatherForecastProvider provider)
+        public WeatherController(IWeatherForecastProvider provider, ICityLogRepository citySearched)
         {
             _provider = provider;
+            _citySearch = citySearched;
         }
 
+        // Get api/weather/Lviv/7
         [Route("api/Weather/{city}/{days}")]
         public IHttpActionResult Get(string city, int days)
         {
             if (string.IsNullOrEmpty(city)) { throw new HttpResponseException(HttpStatusCode.BadRequest); }
-            _provider.GetWeatherForecastJson(city, days, "");
+            if (days< 0 || days > 17) { throw new HttpResponseException(HttpStatusCode.BadRequest); }
 
-            return new StatusCodeResult(HttpStatusCode.OK, this);
+            var cityWeather = _provider.GetWeatherForecastJson(city, days, "");
+            if (cityWeather == null) return NotFound();
+
+            try
+            {
+                _citySearch.AddLog(new CityLog() { CityName = city, RequestTime = DateTime.Now });
+                _citySearch.Save();
+            }
+            catch(Exception)
+            {
+                return InternalServerError();
+            }
+            return Ok(cityWeather);
         }
 
 
