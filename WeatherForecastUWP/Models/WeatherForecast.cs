2547fbdd0e4b3167bac7a88f86a8bc265ac6d522
tyurtyubek <tyurtyubek@outlook.com>
2017-07-30 14:50:18 +0300

    Added UWP project

---
+++ WeatherForecastUWP/Models/WeatherForecast.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace WeatherForecastUWP.Models
+{
+    public class Coord
+    {
+        public double Lon { get; set; }
+        public double Lat { get; set; }
+    }
+
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Coord Coord { get; set; }
+        public string Country { get; set; }
+        public int Population { get; set; }
+    }
+
+    public class Temp
+    {
+        public double Day { get; set; }
+        public double Min { get; set; }
+        public double Max { get; set; }
+        public double Night { get; set; }
+        public double Eve { get; set; }
+        public double Morn { get; set; }
+    }
+
+    public class Weather
+    {
+        public int Id { get; set; }
+        public string Main { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+
+    public class List
+    {
+        public int Dt { get; set; }
+        public Temp Temp { get; set; }
+        public double Pressure { get; set; }
+        public int Humidity { get; set; }
+        public List<Weather> Weather { get; set; }
+        public double Speed { get; set; }
+        public int Deg { get; set; }
+        public int Clouds { get; set; }
+        public double Rain { get; set; }
+    }
+
+    public class RootObject
+    {
+        public City City { get; set; }
+        public string Cod { get; set; }
+        public double Message { get; set; }
+        public int Cnt { get; set; }
+        public List<List> List { get; set; }
+    }
+}
